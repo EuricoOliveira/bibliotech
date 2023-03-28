5338fe55d5d05a9334359d239e242f883e2fb0f4
https://github.com/EuricoOliveira/html.git <https://github.com/EuricoOliveira/html.git>
2023-03-28 10:45:07 -0300

    AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,30 @@ import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
 import { Root } from "./pages/Root/Root";
 import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/config";
+import { AuthContext } from "./contexts/AuthContext";
 
 export function App() {
+
+  const [usuarioLogado, SetUsuarioLogado] = useState(null);
+
+  useEffect(() => {
+    // Monitorar/detectar o usuário conectado
+    onAuthStateChanged(auth, (user) => { // onAuthStateChanged serve para saber se  o usuário esta logado ou não
+      // user é nulo = deslogado
+      // user tem objeto = logado
+      SetUsuarioLogado(user);
+    })
+    // Esse efeito irá rodar apenas uma vez
+    // Quando o app for renderizado
+  }, [])
+
+
   return (
     <>
+      <AuthContext.Provider value={usuarioLogado}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Root />}>
@@ -17,6 +37,7 @@ export function App() {
           <Route path="/cadastro" element={<Cadastro />} />
         </Routes>
       </BrowserRouter>
+      </AuthContext.Provider>
       <Toaster />
     </>
   );
